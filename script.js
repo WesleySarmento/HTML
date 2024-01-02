0153cd68a977489cc03537da530b331ecd83e51b
WesleySarmento <wesleys.494@gmail.com>
2024-01-02 04:36:56 -0300

    update estudo js

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
+/*
+getElementsById -> Trás UM elemento pelo ID
+getElementsByClassName -> Trás TODOS os elementos com essa classe
+getElementsByTagName -> Trás TODOS os elementos com essa TAG
+getElementsByName -> Trás TODOS os elementos com esse NAME
 
-const elements = document.getElementsByTagName("p")
+querySelector -> Trás UM elemento, o PRIMEIRO que encontrar
+querySelectorAll -> Trás TODOS os elementos que encontrar
 
-console.log(elements)
+
+Alterando e Acessando textos
+
+textContent -> Só HTML
+innerText -> Conteúdo sem HTML (apenas texto) leva em conta o CSS
+
+
+*/
+
+const element = document.querySelector("#main-input")
+
+element.placeholder = "Agora é esse texto"
+console.log(element.placeholder)
+
